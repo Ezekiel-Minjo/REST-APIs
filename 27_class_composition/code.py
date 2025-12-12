855ba6480f746fd08a423b4defc61112a5ad2c51
minjo <ezekielminjo4@gmail.com>
2025-12-12 21:51:07 +0300

    class composition

---
+++ 27_class_composition/code.py
@@ -0,0 +1,10 @@
+class BookShelf:
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"Bookshelf with {self.quantity} books"
+
+shelf = BookShelf(300)  
+
+print(shelf)
